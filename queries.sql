b6fa1eb039ead3ffa2ac241ee915b739127d462d
Dennis Every <dennis@ansyra.com>
2023-03-30 14:25:26 -0400

    Inside a transaction update the animals table by setting the species column to digimon for all animals that have a name ending in mon and update the animals table by setting the species column to pokemon for all animals that don't have species already set and commit

--- queries.sql
+++ queries.sql
@@ -15,3 +15,13 @@ BEGIN;
   SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
