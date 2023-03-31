f7fe9b59995de067c3f339e56ee4e381d4fb2833
Dennis Every <dennis@ansyra.com>
2023-03-31 09:46:22 -0400

    modify animals table

--- schema.sql
+++ schema.sql
@@ -22,3 +22,19 @@ CREATE TABLE species(
 	id			INT GENERATED ALWAYS AS IDENTITY,
 	name		VARCHAR(250)
 );
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE species ADD CONSTRAINT uniq_species_id_constraint PRIMARY KEY(id);
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE owners ADD CONSTRAINT uniq_owner_id_constraint PRIMARY KEY(id);
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
